73ea8a9b264e72a6740d748507cf08ab454e042b
Afreen Shakir <79682353+AfreenShakir@users.noreply.github.com>
2021-04-10 17:55:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+import { observer } from "mobx-react";
+import { extendObservable } from "mobx";
+import "./styles.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    extendObservable(this, {
+      counter: 0
+    });
+  }
+
+  onIncrement = () => {
+    this.counter++;
+  };
+
+  onDecrement = () => {
+    this.counter--;
+  };
+
+  render() {
+    return (
+      <div id="mount">
+        {this.counter}
+        <div>
+          <div>
+            <span class="increment">
+              <button onClick={this.onIncrement} type="button">
+                Increment
+              </button>
+            </span>
+            <button onClick={this.onDecrement} type="button">
+              Decrement
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default observer(App);
