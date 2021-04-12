27a6b1e7df0a12a58fa96da6db9e50938ce99632
Afreen Shakir <79682353+AfreenShakir@users.noreply.github.com>
2021-04-12 19:18:55 +0530

    code changes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { storesContext, useStores } from "./store/stores";
 
-import { observer } from "mobx-react";
-import { extendObservable } from "mobx";
-import "./styles.css";
-
-class App extends Component {
-  constructor() {
-    super();
-
-    extendObservable(this, {
-      counter: 0
-    });
-  }
-
-  onIncrement = () => {
-    this.counter++;
-  };
-
-  onDecrement = () => {
-    this.counter--;
-  };
-
-  render() {
-    return (
-      <div id="mount">
-        {this.counter}
-        <div>
-          <div>
-            <span className="increment">
-              <button onClick={this.onIncrement} type="button">
-                Increment
-              </button>
-            </span>
-            <button onClick={this.onDecrement} type="button">
-              Decrement
-            </button>
-          </div>
-        </div>
+function App() {
+  const stores = useStores();
+  return (
+    <storesContext.Provider value={stores}>
+      <div className="App">
+        <Header />
+        <Main />
       </div>
-    );
-  }
+    </storesContext.Provider>
+  );
 }
 
-export default observer(App);
+export default App;
